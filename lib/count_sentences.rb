29628bafe3258c6de09ae1439ab59f6785087df9
Danny Linhardt <daniel.linhardt@gmail.com>
2022-06-29 19:46:39 -0400

    commit tree 62aad0704bf52c3ea945ae758982edec74d97c97

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
-
+  
   def count_sentences
-
+    count = 0
+    self.split.length.times do |i|
+      if self.split[i].sentence? || self.split[i].question? || self.split[i].exclamation?
+        count += 1
+      end
+    end
+    count
   end
+
+
 end
